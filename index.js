cef60c836546434b8fcdff0ebb2dfb455773d54f
9elt <nelt.primary@gmail.com>
2024-06-13 18:50:45 +0200

    added event listener cleanup and FFIE wrapper

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-document.addEventListener("readystatechange", (e) => {
-  if (e.target.readyState !== "complete") {
-    return;
-  }
-
-  const inputs = document.querySelectorAll('input[type="text"], input[type="search"]');
-
-  if (inputs.length === 0) {
-    return;
-  }
-
-  for (let i = 0; i < inputs.length; i++) {
-    if (/search/i.test(inputs[i].outerHTML)) {
-      inputs[i].focus();
+(() => {
+  function focus() {
+    if (document.readyState !== "complete") {
       return;
     }
+
+    const inputs = document.querySelectorAll('input[type="text"], input[type="search"]');
+
+    if (inputs.length === 0) {
+      return cleanup();
+    }
+
+    for (let i = 0; i < inputs.length; i++) {
+      if (/search/i.test(inputs[i].outerHTML)) {
+        inputs[i].focus();
+        return cleanup();
+      }
+    }
+
+    inputs[0].focus();
+    cleanup();
   }
 
-  inputs[0].focus();
-});
+  function cleanup() {
+    document.removeEventListener("readystatechange", focus);
+  }
+
+  document.addEventListener("readystatechange", focus);
+  focus();
+})();
